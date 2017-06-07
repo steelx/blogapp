bfa64bdd787d1c65e9b6acbd683b0d5c147aac91
Steel <ajinkya@riva.co>
2017-06-07 10:44:04 +0530

    blog posts pagination

--- src/app/posts-container/posts-container.component.ts
+++ src/app/posts-container/posts-container.component.ts
@@ -7,12 +7,24 @@ import {PostsService} from '../shared/posts/posts.service';
   styleUrls: ['./posts-container.component.css']
 })
 export class PostsContainerComponent implements OnInit {
+
+  private postsLimit = 5;
   posts$;
   constructor(private postService: PostsService) { }
 
   ngOnInit() {
-    this.posts$ = this.postService.getPosts()
-      .publishReplay().refCount();
+    this.posts$ = this.postService.getAllPosts({query: {
+      limitToFirst: this.postsLimit
+    }})
+      .publishReplay().refCount(); // to avoid multiple subscribe network load
+  }
+
+  nextPosts() {
+
+    this.posts$ = this.posts$.switchMap(posts => {
+      const startAt = posts[posts.length - 1].$key;
+      return this.postService.loadNextPage(startAt, this.postsLimit);
+    });
   }
 
 }
--- src/app/shared/posts/posts.service.ts
+++ src/app/shared/posts/posts.service.ts
@@ -2,21 +2,25 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {AngularFireDatabase} from "angularfire2/database";
 import {Post} from "../model/post";
+import {FirebaseListFactoryOpts} from "angularfire2/interfaces";
+
 
 @Injectable()
 export class PostsService {
 
   constructor(private af: AngularFireDatabase) { }
 
-  getPosts(): Observable<Post[]> {
-    return this.af.list('/posts', {
-        query: {
-          orderByKey: true,
-          limitToFirst: 10
-        }
-      })
-      .map(Post.fromJsonList)
-      .do(console.log);
+  getAllPosts(query: FirebaseListFactoryOpts = {}): Observable<Post[]> {
+    return this.af.list('/posts', query)
+      .map(Post.fromJsonList);
+  }
+
+  loadNextPage(startAt: string, limit = 5) {
+    return this.getAllPosts({query: {
+      orderByKey: true,
+      limitToFirst: 5,
+      startAt
+    }});
   }
 
 
